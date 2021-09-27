b362ab2831297b7b4daf61cd8ba0c587424ff3a8
shikhamishra9 <shikhamishra.jec@gmail.com>
2021-09-27 21:37:35 +0200

    Changes for SCSS

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,9 +5,9 @@ class Footer extends Component {
   render() {
     return (
         <div className="footer">
-          <p>these are some of casie's favorite recipes</p>
+          <p>these are some of Shikha's favorite recipes</p>
           <p>mmm mmm, that's good food! © ℗®™</p>
-          <img src="https://5iyg119fkst3r2lyyhftyvwd-wpengine.netdna-ssl.com/wp-content/uploads/2021/08/casie.jpg" alt="casie with pink hair"/>
+          <img src="https://d1shwc4yijf729.cloudfront.net/resized/700x350/assets/2020/03/04/unnamed_9_5e5f6030a4ef1.jpg" alt="Best Bergen Restraunt"/>
         </div>
   )}
 }
